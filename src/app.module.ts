0d5fc22fa2819dc5d932c0e6de136fc62935e81f
alicebecidiji <103434459+inheritance10@users.noreply.github.com>
2025-12-02 22:43:03 +0300

    first commit for iceberg

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule as AppConfigModule } from './config/config.module';
+
+@Module({
+  imports: [
+    AppConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('database.uri'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
